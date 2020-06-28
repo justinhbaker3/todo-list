519fab8ec251eba4be9d8ed76de99f66f65f6deb
Justin Baker <justin.baker@sendgrid.com>
2020-06-28 15:11:31 -0600

    cmd

---
+++ cmd/todo-list/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/justinhbaker3/todo-list/internal/memorystore"
+	"github.com/justinhbaker3/todo-list/internal/server"
+)
+
+func main() {
+	// Get the port to run the app on from the env variable
+	port := os.Getenv("TODOLIST_PORT")
+	// default to 8080
+	if port == "" {
+		port = "8080"
+	}
+
+	store := memorystore.New()
+	srv := server.New(store)
+
+	router := srv.SetupRoutes()
+
+	addr := fmt.Sprintf(":%s", port)
+	err := router.Run(addr)
+	if err != nil {
+		os.Exit(1)
+	}
+}
